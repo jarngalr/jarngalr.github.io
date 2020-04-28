02697e46e4926e913307895603418edc158db03d
Rahul Jarngal <jarngalr@gmail.com>
2020-04-28 18:13:39 +0530

    game updated

--- mole.js
+++ mole.js
@@ -2,15 +2,18 @@ const img=document.querySelectorAll(".mole-img");
 const worm= document.querySelector(".worm")
 let count=0;
 let points=0;
-
 function randomSeconds(){
     return (Math.floor(Math.random()*18+2))*1000;
 }
 
 function showScore(){
+    count=100;
+    if(points>=100){
+        points=100;
+    }
     let scoreBox=document.querySelector(".score-box");
     scoreBox.style.backgroundColor="crimson";
-    scoreBox.style.animationName="spin";
+    scoreBox.style.animationName="change";
     scoreBox.style.animationDuration="5s"
     scoreBox.style.animationTimingFunction="linear";
     document.querySelector(".score").innerText=`Game Over!
@@ -30,53 +33,70 @@ Score= ${points}`;
 //         }
 //     })
 // }
-
-
-function callHungry(image){
-    image.src="./images/mole-hungry.png";
-    image.classList.add("show-hungry");
-    console.log("Hungry mole Called", Math.floor(Date.now()/1000))
-    if(points<100 && count<100){
-        image.addEventListener("click", function(event){
-                points=points+1;
-                event.currentTarget.classList.remove("show-hungry");
-                event.currentTarget.src="./images/mole-fed.png"
-                image.classList.add("show");
-                // image.classList.add("show");
-                console.log(`Points=`,points);
-                callFed(image);
-        })
+function clickHandler (event){
+    event.target.removeEventListener("click", clickHandler);
+    event.currentTarget.classList.remove("show-hungry");
+    event.target.classList.add("show");
+    
+    if(points<97){
+        points=points+3;
+        console.log(`Points=`,points);
+        callFed(event.target);
+    }else {
+        points=100;
+        showScore();
     }
-    callSad(image);
 }
 
-function callFed(image){
-    setTimeout(() => {
-        // image.classList.add("show");
-        console.log("Fed mole called", Math.floor(Date.now()/1000))
-        callLeaving(image);
-    }, 500);
-}
 
-function callSad(image){
-    
+
+function callHungry(imageH){
+    imageH.style.display="block";
+    imageH.src="./images/mole-hungry.png";
+    imageH.classList.add("show-hungry");
+    console.log("Hungry mole Called", Math.floor(Date.now()/1000))
+
+        imageH.addEventListener("click", clickHandler)
+
     setTimeout(() => {
-        // image.classList.add("show");
-        console.log("Sad mole called", Math.floor(Date.now()/1000))
-        image.src="./images/mole-sad.png"
-        callLeaving(image);
+        imageH.removeEventListener("click", clickHandler);
+        imageH.classList.add("show");
+        imageH.classList.remove("show-hungry");
+        if(imageH.src!="./images/mole-fed.png"){
+            callSad(imageH);
+            return;
+        }
     }, 2000);
+    return;
+}
+
+function callFed(imageF){
+    imageF.src="./images/mole-fed.png";
+    console.log("Fed mole called", Math.floor(Date.now()/1000))
+
+    setTimeout(() => {
+        callLeaving(imageF);
+    }, 500);
+}
+
+function callSad(imageS){
+    imageS.src="./images/mole-sad.png"
+    console.log("Sad mole called", Math.floor(Date.now()/1000));
+
+    setTimeout(() => {        
+        callLeaving(imageS);
+    }, 500);
     
 }
 
-function callLeaving(image){
-    setTimeout(() => {
-        console.log("Leaving mole called", Math.floor(Date.now()/1000))
-        image.src="./images/mole-leaving.png"
-        callDelay(image);
-    }, 500);
+function callLeaving(imageL){
+    console.log("Leaving mole called", Math.floor(Date.now()/1000))
+    imageL.src="./images/mole-leaving.png"
 
-    
+    setTimeout(() => {
+        imageL.style.display="none";
+        callMole(imageL);
+    }, 500);
 }
 
 // function feedKingMole(image){
@@ -92,85 +112,100 @@ function callLeaving(image){
 //         }
 //     })
 // }
-
-function callKingHungry(image){
-    image.src="./images/king-mole-hungry.png";
-    image.classList.add("show-hungry");
-    console.log("Hungry King Mole Called", Math.floor(Date.now()/1000))
-    if(points<100 && count<100){
-        image.addEventListener("click", function(event){
-            points=points+1;
-            event.currentTarget.classList.remove("show-hungry");
-            image.classList.add("show");
-            event.currentTarget.src="./images/king-mole-fed.png"
-            // image.classList.add("show");
-            console.log(`Points=`,points);
-            callKingFed();
-        })
+function clickHandlerK (event){
+    event.target.removeEventListener("click", clickHandlerK);
+    event.target.classList.remove("show-hungry");
+    event.target.classList.add("show");
+      
+    if(points<94){
+        points=points+6;
+        console.log(`Points=`,points);
+        callKingFed(event.target); 
+    }else{
+        points=100;
+        showScore();
     }
-    callKingSad(image);
-   
 }
 
-function callKingFed(image){
-    // image.classList.add("show");
+function callKingHungry(imageKH){
+    imageKH.style.display="block";
+    imageKH.src="./images/king-mole-hungry.png";
+    imageKH.classList.add("show-hungry");
+    console.log("Hungry King Mole Called", Math.floor(Date.now()/1000))
+
+        imageKH.addEventListener("click", clickHandlerK)
+
     setTimeout(() => {
-        // image.classList.remove("show-hungry");
-        console.log("Fed King Mole called", Math.floor(Date.now()/1000))
-        callKingLeaving(image);
+        imageKH.removeEventListener("click", clickHandlerK);
+        imageKH.classList.remove("show-hungry");
+        imageKH.classList.add("show");
+        if(imageKH.src!="./images/king-mole-fed.png"){
+            callKingSad(imageKH);
+        }
+    }, 2000);
+}
+
+function callKingFed(imageKF){
+    imageKF.src="./images/king-mole-fed.png";
+    console.log("Fed King Mole called", Math.floor(Date.now()/1000));
+
+    setTimeout(() => {
+        callKingLeaving(imageKF);
     }, 500);
 }
 
-function callKingSad(image){
-    image.classList.add("show");
+function callKingSad(imageKS){
+    imageKS.src="./images/king-mole-sad.png"
+    console.log("Sad King mole called", Math.floor(Date.now()/1000))
     setTimeout(() => {
-        // image.classList.remove("show-hungry");
-        console.log("Sad King mole called", Math.floor(Date.now()/1000))
-        image.src="./images/king-mole-sad.png"
-        callKingLeaving(image);
-    }, 3000);
+        callKingLeaving(imageKS);
+    }, 500);
     
 }
 
-function callKingLeaving(image){
+function callKingLeaving(imageKL){
+    console.log("Leaving King mole called", Math.floor(Date.now()/1000))
+    imageKL.src="./images/king-mole-leaving.png"
+    
     setTimeout(() => {
-        console.log("Leaving King mole called", Math.floor(Date.now()/1000))
-        image.src="./images/king-mole-leaving.png"
-        callDelay(image);
-    }, 500); 
+        imageKL.style.display="none";
+        callMole(imageKL);
+    }, 500);
 }
 
-function callDelay(image){
-    setTimeout(() => {
-        console.log("Delay called", Math.floor(Date.now()/1000))
-        image.classList.remove("show");
-        image.classList.remove("show-hungry");
-        callMole(image);
-    },500);
-}
+// function callDelay(image){
+//     setTimeout(() => {
+//         console.log("Delay called", Math.floor(Date.now()/1000))
+//         image.classList.remove("show");
+//         image.classList.remove("show-hungry");
+//         callMole(image);
+//     },500);
+// }
 
-function continueGame(image){
+function continueGame(imageC){
 
     console.log("Mole called on time=", Math.floor(Date.now()/1000),`
+
 Mole Count=`,count)
     let random=Math.floor(Math.random()*18+2);
 
     if(random!=5){
-            callHungry(image);
+            callHungry(imageC);
     }else{
-            callKingHungry(image);
+            callKingHungry(imageC);
         return;
     }
 }
 
-function callMole(image){
+function callMole(imageM){
+    worm.style.maxWidth=`1660px`
     worm.style.width = `${points}%`;
     count=count+1;
     setTimeout(() => {
-        if(count<100 && points<=100){
+        if(count<100 && points<100){
             // wormImg.maxWidth="100%"
             // wormImg.width=`${points}%`;
-            continueGame(image);
+            continueGame(imageM);
             return;
         }else{
             showScore();
@@ -179,7 +214,7 @@ Your total points=`,points)
             return;
         }
     }, randomSeconds());
-    
+    return;
 }
 
 function startGame(){
